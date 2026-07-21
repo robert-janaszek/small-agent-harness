ba2e440ec4abfee7e9aff4a0eea0927e4d31f049
robert-janaszek <robert.janaszek@gmail.com>
2026-07-21 22:13:32 +0200

    chore(config): add setup file for Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['./src/test/setup.ts'],
     exclude: ['dist/**', 'node_modules/**', '**/*.system.test.ts'],
     coverage: {
       provider: 'v8',
--- vitest.system.config.ts
+++ vitest.system.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.system.test.ts'],
     exclude: ['dist/**', 'node_modules/**'],
     testTimeout: 120_000,
